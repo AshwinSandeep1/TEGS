f0259cb0ebaaab8a31649dbfe02fb9eb8e30541d
Ashwin Bharath Sandeep <135298716+AshwinSandeep1@users.noreply.github.com>
2024-07-20 21:49:49 +0100

    Delete Run files/batchcode_base_model.sh

--- Run files/batchcode_base_model.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-#SBATCH -n 1 
-#SBATCH -N 1
-#SBATCH -p sched_mit_ase 
-cd "/nobackup1/asandeep"
-module add julia/1.8.5
-julia --project=. /nobackup1/asandeep/TEGS_runs/run_files/comparison_modeling_methods/Run_base_model.jl
